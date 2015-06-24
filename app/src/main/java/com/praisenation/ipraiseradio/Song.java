4ada514cf901a2743be7936c262d5aebbe1d1ef5
kayanhales <daybreakbliss@gmail.com>
2015-06-24 00:46:29 -0400

    step 3 - code.tutsplus

---
+++ app/src/main/java/com/praisenation/ipraiseradio/Song.java
@@ -0,0 +1,24 @@
+package com.praisenation.ipraiseradio;
+
+/**
+ * Created by kayanhales on 6/24/15.
+ */
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+
+
+}
